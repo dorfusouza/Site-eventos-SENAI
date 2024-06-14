988c6ec92ec83eb1243c9edfac83c467530ced51
Carlos Alexandre Cavalheiro <58004969+CarlosCavalheiro@users.noreply.github.com>
2024-06-14 07:10:06 -0300

    Update QrPage.jsx

--- src/projeto_portaria/pages/QrPage/QrPage.jsx
+++ src/projeto_portaria/pages/QrPage/QrPage.jsx
@@ -9,14 +9,13 @@ import QrReader from "react-qr-scanner";
 
 const QrPage = () => {
   const navigate = useNavigate();
-  const [cameraAtivada, setCameraAtivada] = useState(true);
+  const [cameraAtivada, setCameraAtivada] = useState(false);
   const [loading, setLoading] = useState(false);
   const [resultadoQrCode, setResultadoQrCode] = useState("");
+  const [facingMode, setFacingMode] = useState("environment");
 
   const inDevelopment = localStorage.getItem("inDevelopment");
   const url = inDevelopment === "true" ? constantes.localApiUrl : constantes.apiUrl;
-
-  const videoRef = useRef(null);
 
   useEffect(() => {
     if (!isAuthenticated()) {
@@ -27,11 +26,10 @@ const QrPage = () => {
 
   const handleScan = (data) => {
     if (data) {
-      console.log(data.text);
-      setResultadoQrCode(data.text);
+      setResultadoQrCode(data);
       setLoading(true);
       setCameraAtivada(false);
-      scanResultado(data.text);
+      scanResultado(data);
     }
   };
 
@@ -71,9 +69,9 @@ const QrPage = () => {
     <>
       <Cabecalho />
       <div className="container d-flex mb-5 mt-5 flex-column align-items-center justify-content-center pt-5 pb-5" style={{ paddingBottom: "600px" }}>
-        <div className="container justify-content-center">
+        <div className="col justify-content-center">
           <div className="col text-center">
-            <h1 className="p-4">Aponte o QR CODE</h1>
+            <h2 className="p-4">Aponte para o QRCODE</h2>
             <div className="bloco_czc pb-5 d-flex justify-content-center">
               <div className="camera-container">
                 <QrReader
@@ -81,8 +79,11 @@ const QrPage = () => {
                   onError={handleError}
                   onScan={handleScan}
                   style={{ width: "100%", height: "100%" }}
+                  constraints={{
+                    video: { facingMode: facingMode }
+                  }}
                 />
-                {cameraAtivada && <div className="qr-overlay"></div>}
+                {<div className="qr-overlay"></div>}
                 {loading && (
                   <div className="loading-section">
                     <div className="loading-overlay d-flex justify-content-center align-items-center">
@@ -90,12 +91,16 @@ const QrPage = () => {
                         <span className="visually-hidden">Loading...</span>
                       </div>
                     </div>
-                    
+                    <div className="loading-text mt-3">Aguarde, verificando ingresso...</div>
                   </div>
                 )}
               </div>
             </div>
             <div id="resultadoQrCode" className="mt-2">{resultadoQrCode}</div>
+            <div className="camera-toggle-buttons">
+              <button onClick={() => setFacingMode("user")} className="btn btn-outline-success m-2">Câmera Frontal</button>
+              <button onClick={() => setFacingMode("environment")} className="btn btn-outline-success m-2">Câmera Traseira</button>
+            </div>
           </div>
         </div>
       </div>
